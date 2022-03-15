9635eb71f948b8e659e54544e5b121b9143e02c7
thu yein aung <thu yein aung>
2022-03-16 00:11:30 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Auth from "aws-amplify";
+import awsConfig from "./aws-exports";
+import "@aws-amplify/ui-vue/styles.css";
 
+Auth.configure(awsConfig);
 createApp(App).mount('#app')
